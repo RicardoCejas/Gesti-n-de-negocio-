491a40d99513c49cfbe71206f1ef298a9ff026a9
facundo mora <facumora3103@gmail.com>
2024-10-23 16:04:21 -0300

    ultimos modificaciones

--- efectivo.py
+++
@@ -1,7 +0,0 @@
-# Clase para pagos con Efectivo
-
-from metodos_pago import MetodoDePago
-
-class Efectivo(MetodoDePago):
-    def procesar_pago(self, monto):
-        print(f"Pago de {monto} procesado con Efectivo.")
--- main.py
+++
@@ -1,8 +0,0 @@
-from pagar import realizar_pago
-from crear_productos import menu
-
-if __name__ == "__main__":
-    realizar_pago()  # Llama a la función realizar_pago desde el archivo pagar.py
-
-if __name__ == "__main__":
-    menu()
